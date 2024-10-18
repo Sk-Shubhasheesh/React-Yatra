dfdb8e65304d6e03f8497b27adf776ec956d2f0d
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-10-18 19:12:28 +0530

    Conditional Rendering in React

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //inject js inside the jsx - dynamiclly update element
-const apiData =  "heelo from api data"
-const isMorning = true
+// const apiData =  "heelo from api data"
+
 
 // const div = <div className="text">Hello from JSX</div>
-const div = <div className="text">Hello Ji {apiData}</div>
+// const div = <div className="text">Hello Ji {apiData}</div>
+
+                                             // Conditional Rendering
+const isMorning = false
+const morningText = "Good Morning User"
+const afternoonText = "Good after noon User"
+// const div = <div className="text">{isMorning ? morningText:afternoonText}</div>
+
+// IF we want to put some value with conditonal rendering
+const morningElement = <div>{morningText} <span>it is 8am</span></div>
+const eveningElement = <div>{afternoonText} it is 4pm</div>
+const div = isMorning ? morningElement : eveningElement
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(div)
