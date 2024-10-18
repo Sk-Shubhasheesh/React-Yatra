8e3235567f878a20b656b3520346758bc11c6c94
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-10-18 11:32:49 +0530

    Hello world using ReactJs

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const root  = document.getElementById('root');
-const p = document.createElement('p');
-p.innerHTML = "Hello from Shubhasheesh using Js";
-root.appendChild(p);
+// Create Single element
+
+// const div = React.createElement('div', {className:'text'}, "Hello from Shubhasheesh using React")
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(div)
+
+
+// Add childm tp parent
+// const child = React.createElement('p', {}, "I am child of div(text) element")
+// const div = React.createElement('div', {className:'text'}, child)
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(div)
+
+// sibling hierarchy
+const child1 = React.createElement('p', {}, "I am child 1 of div(text) element")
+const child2 = React.createElement('p', {}, "I am child 2 of div(text) element")
+const div = React.createElement('div', {className:'text'}, [child1, child2])
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(div)
