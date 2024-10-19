4e29926eac756f24a35bf95bfafbb53f39bdcd6d
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-10-19 16:14:05 +0530

    useState Example-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
-import { FruitList, StudentList, StudentInfo } from "./renderDataList";
-
+import { useState } from "react";
 function App(){
-  const student = {
-    id: 1,
-    name: "John Doe",
-    age: 20,
-    course: "Computer Science",
-    year: 2
-  };
+  const [message, updateMessage] = useState('Hello user, good morning')
+  const changeMessage = ()=>{
+    updateMessage('Hello user, good afternoon')
+  }
+  return <>
+    <div>{message}</div>
+    <button onClick={changeMessage}>Change Message</button>
+    </>
 
-  return(
-    <div>
-      <h1>Fruit List</h1>
-      <FruitList /> {/* Renders the list of fruits */}
-      <h1>Student List</h1>
-      <StudentList />
-      <h1> Rendering Data from an Object Passed via Props</h1>
-      <StudentInfo student={student} />
-    </div>
-  )
 }
 export default App;
