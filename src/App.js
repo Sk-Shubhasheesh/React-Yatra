d63f3c25b752eb6e11b43288a8c50ff3f7888625
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-10-19 10:02:46 +0530

    Import/exports

---
+++ src/App.js
@@ -0,0 +1,10 @@
+const App = ()=>{
+    return "hello"
+}
+
+
+// default exports - you can have only one default exports
+  // export default App;
+
+// named exports
+export {App}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+// import App from "./App";  // default imports
+import { App } from "./App";  // named imports
 
-//inject js inside the jsx - dynamiclly update element
-// const apiData =  "heelo from api data"
 
-
-// const div = <div className="text">Hello from JSX</div>
-// const div = <div className="text">Hello Ji {apiData}</div>
-
-                                             // Conditional Rendering
-const isMorning = false
-const morningText = "Good Morning User"
-const afternoonText = "Good after noon User"
-// const div = <div className="text">{isMorning ? morningText:afternoonText}</div>
-
-// IF we want to put some value with conditonal rendering
-const morningElement = <div>{morningText} <span>it is 8am</span></div>
-const eveningElement = <div>{afternoonText} it is 4pm</div>
-const div = isMorning ? morningElement : eveningElement
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(div)
+
+root.render(<App />)
