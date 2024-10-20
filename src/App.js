1a27eb2e95619292cc39e41804ec4b6001d5525d
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-10-20 11:08:47 +0530

    Understanding useRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import LayoutEffectExample from './LayoutEffect';
+import React, { useRef } from 'react';
+import { useState } from 'react';
 
-function App() {
-  return(
-       <LayoutEffectExample />
-  )
+
+
+const App = ()=>{
+  let myLocal = 0
+  const ref = useRef(0)
+  const [myCount, setCount] = useState(0)
+  return <>
+  <button onClick={() => {myLocal+= 1}}>Change Local Variable</button>
+   <button  onClick={() => {ref.current+= 1}}>Change Ref</button>
+   <button  onClick={() => {setCount(myCount+1)}}>Change State</button>
+
+   <div>
+    <span>Local var: {myLocal}</span><br/>
+    <span>ref: {ref.current}</span><br/>
+    <span>state variable: {myCount}</span><br/>
+   </div>
+  </>
+
 }
 
 export default App;
